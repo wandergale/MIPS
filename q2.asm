db115fc468d376fd31469ad0444e8ab4e3d8eac9
Wanderson <wandersongamer2@gmail.com>
2022-11-08 16:34:52 -0300

    A[300] = h + A[300] in ASSEMBLY

--- q2.asm
+++ q2.asm
@@ -1,15 +1,22 @@
 .data
+	Resultado: .asciiz "Resultado: "
+	Igual: .asciiz "Igual" # testando condição
 	myArray:
 		.align 2
 		.space 1204
+		
 .text
 	move $t0, $zero
 	move $t1, $zero
 	li $t2, 1204
 	
 	li $t3, 15
+	li $t4, 300
+	lw $t5, myArray($t4)
 	
 	loop:
+		beq $t4, $t5, soma
+		
 		beq $t0, $t2, endLoop
 		sw $t1, myArray($t0)
 		addi $t0, $t0, 4
@@ -18,15 +25,20 @@
 		j loop
 	
 	endLoop:
-		move $t0, $zero
-		print:
-			beq $t0, $t2, endPrint
-			li $v0, 1
-			lw $a0, myArray($t0)
-			syscall
-			
-			addi $t0, $t0, 4
-			j print
-		endPrint:
+		# pode colocar funcao de printar aqui
 					
-	
+	soma:
+		add $t6, $t3, $t5
+		sw $t6, myArray($t4)
+		
+		lw $t5, myArray($t4)
+		
+		
+		li $v0, 4
+		la $a0, Resultado
+		syscall
+		
+		li $v0, 1
+		move $a0, $t5
+		syscall
+		
