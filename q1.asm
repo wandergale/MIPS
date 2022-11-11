e17f2e0b8ba6c02999b5bc48920e146261990f7c
Wanderson <wandersongamer2@gmail.com>
2022-11-11 20:53:54 -0300

    comentando

--- q1.asm
+++ q1.asm
@@ -8,11 +8,11 @@
 	add $t0, $s1, $s2
 	# t0 = 55 + 45
 	
-	li $v0, 4 # codigo pra printar string
+	li $v0, 4 # codigo para printar string
 	la $a0, Resultado
 	# printa a mensagem
 	syscall
  
-	li $v0, 1 # codigo pra printar inteiro
+	li $v0, 1 # codigo para printar inteiro
 	move $a0, $t0
 	syscall
